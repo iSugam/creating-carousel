9049ee4dfbb46c568f84ecb2f9a11894f4e254b8
unknown <sugamanandsahu01@gmail.com>
2022-08-10 08:24:26 +0530

    Adding carousel function for carousels.

---
+++ main.js
@@ -0,0 +1,15 @@
+carousel({
+    nextButton: ".next",
+    prevButton: ".prev",
+    item: ".item",
+    style: {
+        transition: 200
+    }
+})
+
+carousel({
+    nextButton: ".nextBtn",
+    prevButton: ".prevBtn",
+    item: ".item2",
+    transition: 300
+})
