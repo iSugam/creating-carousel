d08c3432364d04936628d8d6c2b51537cd521c22
unknown <sugamanandsahu01@gmail.com>
2022-08-10 08:23:08 +0530

    Making custom function for carousel.

---
+++ slider.js
@@ -0,0 +1,55 @@
+let curSlide = 0
+
+const carousel = ({nextButton, prevButton, item, transition = 100, delay = 0, width}) => {
+
+    // Get the items from document to slide
+    const newItem = document.querySelectorAll(item)
+
+    let slideItem;
+    // Get the next Button
+    const nextBtn = document.querySelector(nextButton)
+    nextBtn.addEventListener("click", () => {
+        curSlide--;
+
+        setTimeout(() => {
+            for(let i = 0; i < newItem.length; i++) {
+                slideItem = newItem[i]
+    
+                // custon transition for sliders
+                slideItem.style.transition = transition + "ms"
+        
+                if(curSlide < -(newItem.length - 1)) {
+                    curSlide = 0
+                    slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+                }
+                slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+            }
+        }, delay)
+
+    })
+
+    // Get the previous Button
+    const prevBtn = document.querySelector(prevButton)
+    prevBtn.addEventListener("click", () => {
+
+        curSlide++;
+
+        setTimeout(() => {
+
+            for(let i = 0; i < newItem.length; i++) {
+                slideItem = newItem[i];
+
+                // custon transition for sliders
+                slideItem.style.transition = transition + "ms"
+    
+                if(curSlide > 0) {
+                    curSlide = -(newItem.length - 1)
+                    slideItem.style.transform = `translateX(${100 * curSlide}%)`;
+                }
+                slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+            }
+        }, delay)
+
+    })
+
+}
