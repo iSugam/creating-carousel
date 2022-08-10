a983108a7420ae5d6090dac78441f592d3f67f24
unknown <sugamanandsahu01@gmail.com>
2022-08-10 09:42:56 +0530

    Adding comments.

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
-let curSlide = 0
+// Deault currentNumber is 0
+let currentSlide = 0
 
 const carousel = ({
     nextButton, 
@@ -15,21 +16,30 @@ const carousel = ({
     // Get the next Button
     const nextBtn = document.querySelector(nextButton)
     nextBtn.addEventListener("click", () => {
-        curSlide--;
+        currentSlide--;
 
         setTimeout(() => {
+            // Looping through all the items to get the next Item
             for(let i = 0; i < newItem.length; i++) {
                 slideItem = newItem[i]
     
                 // custon transition for sliders
                 slideItem.style.transition = transition + "ms";
                       
-                console.log(slideItem.style);
-                if(curSlide < -(newItem.length - 1)) {
-                    curSlide = 0
-                    slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+                /* 
+                    If the currentSlide number is less than the length 
+                    of newItem.length - 1 in negative 
+                    (for example: lets say length of newItem is 10, so 10-1 = 9, 
+                    with each click it will decreace in negative fron 0 to -9)
+                    then set it to 0 which willbe the first item of the newItem
+                */
+               console.log(currentSlide);
+
+                if(currentSlide < -(newItem.length - 1)) {
+                    currentSlide = 0
+                    slideItem.style.transform = `translateX(${100 * (currentSlide)}%)`;
                 }
-                slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+                slideItem.style.transform = `translateX(${100 * (currentSlide)}%)`;
             }
         }, delay)
 
@@ -37,23 +47,34 @@ const carousel = ({
 
     // Get the previous Button
     const prevBtn = document.querySelector(prevButton)
-    prevBtn.addEventListener("click", () => {
 
-        curSlide++;
+    /* 
+        Adding click event to the previous button
+        to change the carousel item to previous item 
+    */
+    prevBtn.addEventListener("click", () => {
+        currentSlide++;
 
         setTimeout(() => {
-
+            // Looping through all the items to get the previous Item
             for(let i = 0; i < newItem.length; i++) {
                 slideItem = newItem[i];
 
                 // custon transition for sliders
                 slideItem.style.transition = transition + "ms"
     
-                if(curSlide > 0) {
-                    curSlide = -(newItem.length - 1)
-                    slideItem.style.transform = `translateX(${100 * curSlide}%)`;
+                /* 
+                    If the currentSlide number is greater than 0
+                    then change the currentNumber to the newItem.length - 1 in negative
+                    (for example: lets say the length of newItem is 10, so 10-1 = 9, 
+                    after turning to negative it's -9 and with each click it will increase from -9 to 0)
+                    then set it to -9 which is the last item of newItem
+                */
+                if(currentSlide > 0) {
+                    currentSlide = -(newItem.length - 1)
+                    slideItem.style.transform = `translateX(${100 * currentSlide}%)`;
                 }
-                slideItem.style.transform = `translateX(${100 * (curSlide)}%)`;
+                slideItem.style.transform = `translateX(${100 * (currentSlide)}%)`;
             }
         }, delay)
 
