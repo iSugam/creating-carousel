d7206d51692377eab4e3637a71c4d71ceefb9ff7
unknown <sugamanandsahu01@gmail.com>
2022-08-10 19:36:58 +0530

    Adding new parameters.

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ carousel({
     prevButton: ".prev", // Previous Button Class Name
     sliderItems: ".item", // class name of Carousel items
     transition: 300, // For CSS transition
-    delay: 0 // Default is 0
+    delay: 0, // Default is 0
+    itemsToShow: {
+        largeDevice: 1, // 1 sliderItems in desktop or large devices
+    }
 })
 
 
@@ -14,5 +17,10 @@ carousel({
     prevButton: ".prevBtn", // Previous Button Class Name
     sliderItems: ".item2", // class name of Carousel items
     transition: 600, // For CSS transition
-    delay: 0 // Default is 0
+    delay: 0, // Default is 0
+    itemsToShow: {
+        largeDevice: 5, // 5 sliderItems in desktop or large devices
+        mediumDevece: 3, // 3 sliderItems in tab or medium devices
+        smallDevice: 2 // 2 sliderItems in small or mobile devices
+    }
 })
