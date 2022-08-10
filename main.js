480d057e2e561b53f48f73977d28b87371301661
unknown <sugamanandsahu01@gmail.com>
2022-08-10 09:52:57 +0530

    Adding comments.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // For FIRST carousel
 carousel({
-    nextButton: ".next",
-    prevButton: ".prev",
-    SliderItems: ".item",
-    transition: 300,
+    nextButton: ".next", // Next button class name
+    prevButton: ".prev", // Previous Button Class Name
+    SliderItems: ".item", // class name of Carousel items
+    transition: 300, // For CSS transition
+    delay // Default is 0
 })
 
 
 // For SECOND Carousel
 carousel({
-    nextButton: ".nextBtn",
-    prevButton: ".prevBtn",
-    SliderItems: ".item2",
-    transition: 1000
+    nextButton: ".nextBtn", // Next button class name
+    prevButton: ".prevBtn", // Previous Button Class Name
+    SliderItems: ".item2", // class name of Carousel items
+    transition: 1000, // For CSS transition
+    delay // Default is 0
 })
