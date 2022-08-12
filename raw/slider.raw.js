06bbb3e36c4b285f05f57c45d010ab7f54fa0a8d
unknown <sugamanandsahu01@gmail.com>
2022-08-12 20:54:29 +0530

    updating the raw/un-commented file.

--- raw/slider.raw.js
+++ raw/slider.raw.js
@@ -7,24 +7,18 @@ const carousel = ({
     autoSlide = true,
     autoSlideDelay = 5
 }) => {
-
     let currentSlide = 0
     const sliderItem = document.querySelectorAll(sliderItems);
-
+    const carouselContainer = sliderItem[0].parentElement
     let slideItem;
     let slideInterval;
-    const setLength = Math.round(sliderItem[0].parentElement.offsetWidth / sliderItem[0].offsetWidth);
-
+    const setLength = Math.round(carouselContainer.offsetWidth / sliderItem[0].offsetWidth);
     const GET_NEXT = () => {
         currentSlide--;
-
         setTimeout(() => {
-           
             for(let i = 0; i < sliderItem.length; i++) {
                 slideItem = sliderItem[i]
-
                 slideItem.style.transition = transition + "ms";
-
                 if(currentSlide < -(sliderItem.length - setLength)) {
                     currentSlide = 0;
                     slideItem.style.transform = `translateX(${100 * (currentSlide)}%)`;
@@ -33,16 +27,12 @@ const carousel = ({
             }
         }, delay)
     }
-
     const GET_PREVIOUS = () => {
         currentSlide++;
-
         setTimeout(() => {
             for(let i = 0; i < sliderItem.length; i++) {
                 slideItem = sliderItem[i];
-
                 slideItem.style.transition = transition + "ms"
-    
                 if(currentSlide > 0) {
                     currentSlide = -(sliderItem.length - setLength);
                     slideItem.style.transform = `translateX(${100 * currentSlide}%)`;
@@ -50,14 +40,13 @@ const carousel = ({
                 slideItem.style.transform = `translateX(${100 * (currentSlide)}%)`;
             }
         }, delay)
-
     }
 
-    document.querySelector(nextButton).addEventListener("click",GET_NEXT)
-    document.querySelector(prevButton).addEventListener("click", GET_PREVIOUS)
-
-    const startSlide = () => {slideInterval = setInterval(GET_NEXT, autoSlideDelay * 1000)}
-    if(autoSlide === true) {startSlide()}
-    sliderItem[0].parentElement.addEventListener("mouseenter", () => {clearInterval(slideInterval)})
-    sliderItem[0].parentElement.addEventListener("mouseleave", startSlide)
+    document.querySelector(nextButton).addEventListener("click", GET_NEXT);
+    document.querySelector(prevButton).addEventListener("click", GET_PREVIOUS);
+    const startSlide = () =>  slideInterval = setInterval(GET_NEXT, autoSlideDelay * 1000)
+    if (autoSlide === false) return clearInterval(slideInterval)
+    if(autoSlide === true) startSlide()
+    carouselContainer.addEventListener("mouseenter", () => clearInterval(slideInterval))
+    carouselContainer.addEventListener("mouseleave", startSlide)
 }
